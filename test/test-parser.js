a790487b9699d78dc330b02c1e99d078e4bb8420
snypelife <brettmrogerson@gmail.com>
2015-04-29 13:13:45 -0400

    fix currency parsing by decoding string at the end

--- test/test-parser.js
+++ test/test-parser.js
@@ -24,13 +24,13 @@ describe('parser', function () {
 
   describe('traverse()', function () {
     it('should traverse complex properties with custom routing', function () {
-      expect(parser.traverse(dictionary, 'routed', [[2]]))
+      expect(parser.traverse(dictionary, 'routed', [2]))
       .to
       .equal('this is a plural(%d) statement');
     });
 
     it('should traverse nested complex properties with custom routing', function () {
-      expect(parser.traverse(dictionary, 'routed', [[1], [1]]))
+      expect(parser.traverse(dictionary, 'routed', [1]))
       .to
       .equal('this is a nested singular(%d) statement');
     });
