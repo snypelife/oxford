d9bc98fd5ac055b2c5b4a7cc09ca66ad5f3c0148
snypelife <brettmrogerson@gmail.com>
2015-07-06 12:35:36 -0400

    only build once, only resolve once

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ var builder = require('./lib/builder');
 var get = require('./lib/get');
 var t = require('traverse');
 
-function oxford(dict, childPath) {
+function oxford(dict) {
   var dictionary = builder.build(dict);
-  if (childPath) {
-    if (!Array.isArray(childPath)) {
-      childPath = childPath.split('.');
+
+  function buildChild(childPath) {
+    var dict = dictionary;
+
+    if (childPath) {
+      if (!Array.isArray(childPath)) {
+        childPath = childPath.split('.');
+      }
+      dict = t(dict).get(childPath) || {};
     }
-    dictionary = t(dictionary).get(childPath) || {};
+
+    return {
+      dictionary: dict,
+      get: get.bind(null, dict),
+      child: buildChild
+    };
   }
-  return {
-    dictionary: dictionary,
-    get: get.bind(null, dictionary),
-    child: function(path) {
-      return oxford(dictionary, path);
-    }
-  };
+
+  return buildChild();
 }
 
 module.exports = oxford;
