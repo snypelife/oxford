98b2509e7d8b85aba00c5851aee80da84b1afd1f
darrenmce <dmcelligott@360incentives.com>
2015-07-06 16:25:03 -0400

    fixed some funky closure behaviour
      - was causing the original dictionary to be used as context in 2nd+ 'child' call

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ var builder = require('./lib/builder');
 var get = require('./lib/get');
 var t = require('traverse');
 
+
+function buildChild(dictionary, childPath) {
+  var child = dictionary;
+
+  if (childPath) {
+    if (!Array.isArray(childPath)) {
+      childPath = childPath.split('.');
+    }
+    child = t(child).get(childPath) || {};
+  }
+
+  return {
+    dictionary: child,
+    get: get.bind(null, child),
+    child: buildChild.bind(null, child)
+  };
+}
+
 function oxford(dict) {
   var dictionary = builder.build(dict);
 
-  function buildChild(childPath) {
-    var child = dictionary;
-
-    if (childPath) {
-      if (!Array.isArray(childPath)) {
-        childPath = childPath.split('.');
-      }
-      child = t(child).get(childPath) || {};
-    }
-
-    return {
-      dictionary: child,
-      get: get.bind(null, child),
-      child: buildChild
-    };
-  }
-
-  return buildChild();
+  return buildChild(dictionary);
 }
 
 module.exports = oxford;
